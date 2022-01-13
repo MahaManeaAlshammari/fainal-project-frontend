e24345e93c48d7c45724885e72a94ce582b4bcf8
mahaManae <johndoe@example.com>
2022-01-13 17:06:04 +0300

    f

--- src/components/ReedAthkar.js
+++ src/components/ReedAthkar.js
@@ -7,6 +7,10 @@ export default function ReedAthkar({ token ,Admin }) {
     const [reedThkr, setReedThkr] = useState([])
     const [dataAdmit, setDataAdmit] = useState("")
     const [deletThkr, setDeletThkr] = useState("")
+    const [description, setdescription] = useState("")
+    const [namee, setnamee] = useState("")
+
+    const [type, settype] = useState("")
 
     // عرفت ستيت تخزن قيمه البيانات اللي جت من المونقو
     const{name} = useParams()
@@ -40,28 +44,61 @@ export default function ReedAthkar({ token ,Admin }) {
     };
     ///////////////////////admin/////////////////////////////
     const changeAdmin = (e) => {
-      setDataAdmit(e.target.value);
+      setdescription(e.target.value);
     };
-
+    const changeAdminN = (e) => {
+      setnamee(e.target.value);
+    };
+    const changeAdminT = (e) => {
+      settype(e.target.value);
+    };
+    
     const addathkar = async (id) => {
       try {
-        const result = await axios.post(`http://localhost:5000/ReedAthkar/${id}`,{},{
+        const result = await axios.post(`http://localhost:5000/athkar`,{namee:name , description ,type },{
             headers: { authorization: "Bearer " + token },
           }
         );
-        console.log("dataAdmit",dataAdmit);
+        const copy = [...reedThkr]
+        setReedThkr(copy)
+        console.log("dataAdmit",result.data);
 
       } catch (error) {
       }
     };
-    const deleetThkr= async (id) => {
-      const result = await axios.delete(`http://localhost:5000/ReedAthkar/${id}`, {
+    // const deleetThkr= async (id, index) => {
+    //   const result = await axios.delete(`http://localhost:5000/athkar/${id}`, {
+    //     headers: { authorization: "Bearer " + token },
+    //   });
+    //   console.log("delete" ,result.data);
+    //   // setDeletThkr(result.data);
+    //   // console.log("id : ",id  ,"token : ",token);
+    //   if (deleetThkr.data) {
+    //     const copyThker = [...reedThkr];
+    //     console.log("ooooooooooooooo");
+    //     copyThker.splice(index, 1);
+    //     setReedThkr(copyThker);
+    //   }
+    // }
+    
+
+
+    const deleetThkr = async (id, index) => {
+      console.log("jjjjjjjjjjj");
+      const deletes = await axios.delete(`http://localhost:5000/athkar/${id}`,{
         headers: { authorization: "Bearer " + token },
-      });
-      setDeletThkr(result.data);
-      
+  
+       });
+       console.log("hhhhhh");
+      if (deletes.data) {
+        const copyThker = [...reedThkr];
+        console.log("ooooooooooooooo");
+        copyThker.splice(index, 1);
+        setReedThkr(copyThker);
+      }
     }
-    // ////
+      
+  
 
 
     
@@ -69,18 +106,30 @@ export default function ReedAthkar({ token ,Admin }) {
     return (
       <div>
         
-    {reedThkr.map((elem,i)=>{
-        return(<div>
-            <p className="name-css">{elem.description}            
-             <button onClick={()=>{addFavorite(elem._id)}}>  📌 </button> </p>
+        {Admin==true ? <>
+        <input  onChange={(e) => {changeAdmin(e);}}placeholder="add athkar"/>
+        <input  onChange={(e) => {changeAdminN(e);}}placeholder="nn"/>
+        <input  onChange={(e) => {changeAdminT(e);}}placeholder="tt"/>
 
-             {Admin==true ? <><p><input  onChange={(e) => {changeAdmin(e);}}placeholder="add athkar"/></p>
-           <p>  <button onClick={()=>{addathkar(elem._id)}}>  اضافه </button> </p>
-           <p>  <button onClick={()=>{deleetThkr(elem._id)}}>  حذف </button> </p>
-           <p>  <button onClick={()=>{addathkar(elem._id)}}>  تعديل </button> </p>
+           <p>  <button onClick={()=>{addathkar(reedThkr._id)}}>  اضافه </button> </p>
+          
 
            </> :""}
-            
+    {reedThkr.map((elem,i)=>{
+        return(<div>
+            <p className="name-css">
+                
+             {Admin==true ? <>
+           <button onClick={()=>{deleetThkr(elem._id)}}>  حذف </button> 
+           <button onClick={()=>{addathkar(elem._id)}}>  تعديل </button> 
+
+           </> :""}
+              
+              {elem.description}            
+             <button onClick={()=>{addFavorite(elem._id)}}>  📌 </button>  
+           
+            </p>
+
         </div>)
     })}
       </div>
